895505767b64fd20fed6363e42febe2470a2589d
Baturpcu <batusalkim11@gmail.com>
2022-12-24 16:59:16 +0300

    last update
    last update

--- personal_portfolio_website/hex.js
+++ personal_portfolio_website/hex.js
@@ -13,3 +13,50 @@ sidebarBtn.addEventListener("click", function(){
     elementToggleFunc(sidebar);
 })
 
+//Experiences variables
+const testimonialsItem = document.querySelectorAll("[data-testimoials-item]");
+const modalContainer = document.querySelector("[data-modal-container]");
+const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
+const overlay = document.querySelector("[data-overlay]");
+
+//languages veriables
+
+const modalImg = document.querySelector("[data-modal-img]");
+const modalTitle = document.querySelector("[data-modal-title]");
+const modalText = document.querySelector("[data-modal-text]");
+
+
+//languages function
+
+const testimonialsModalFunc = function(){
+    modalContainer.classList.toggle("active");
+    overlay.classList.toggle("active");
+}
+
+for(let i=0; i<testimonialsItem.length; i++){
+
+    testimonialsItem[i].addEventListener("click",function(){
+        modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
+        modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt,
+        modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
+        modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
+
+        testimonialsModalFunc();
+    });
+}
+modalCloseBtn.addEventListener("click",testimonialsModalFunc);
+overlay.addEventListener("click",testimonialsModalFunc);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
